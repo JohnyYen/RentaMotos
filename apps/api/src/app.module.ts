f473b2f7c20f816618f5d65f2e4607d06f847352
JohnyYen <126342433+JohnyYen@users.noreply.github.com>
2024-08-12 23:20:49 -0400

    Estructura basica de la api completa

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { ContractModule } from './contract/contract.module';
+import { MotorcycleModule } from './motorcycle/motorcycle.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, ContractModule, MotorcycleModule],
   controllers: [AppController],
   providers: [AppService],
 })
