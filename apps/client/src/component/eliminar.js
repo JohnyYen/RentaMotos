45aaeb7499e038d06070e7eadc16891b47702cb3
Gab-LRL <149244493+Gab-LRL@users.noreply.github.com>
2024-08-18 13:27:28 -0400

    ,

---
+++ apps/client/src/component/eliminar.js
@@ -0,0 +1,33 @@
+import{ Button}from "antd";
+import './App.css';
+
+
+
+const EliminarElemento = () => {
+  return (
+
+  <div className="Eliminar">
+  
+      <div className="Mensaje" >
+        ¿Seguro que desea eliminar este elemento?
+      </div>
+
+      <div  className="Botones"  >
+        <Button className="B1" type="primary" htmlType="submit" block >Cancelar
+        </Button>
+
+        <Button className="B2"
+          type="primary" htmlType="submit" block
+
+        >
+          Aceptar
+        </Button>
+      </div>
+  
+  
+  </div>  
+  
+  );
+};
+
+export default  EliminarElemento;
