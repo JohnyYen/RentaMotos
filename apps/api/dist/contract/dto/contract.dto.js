1d620abd82eec14ffa18aeca8f28c3bbf7dec1c7
JohnyYen <126342433+JohnyYen@users.noreply.github.com>
2024-09-09 12:37:55 -0400

    Listo para el merge

--- apps/api/dist/contract/dto/contract.dto.js
+++ apps/api/dist/contract/dto/contract.dto.js
@@ -1,50 +1,7 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ContractDto = void 0;
-const class_validator_1 = require("class-validator");
 class ContractDto {
 }
 exports.ContractDto = ContractDto;
-__decorate([
-    (0, class_validator_1.IsString)(),
-    __metadata("design:type", String)
-], ContractDto.prototype, "idCliente", void 0);
-__decorate([
-    (0, class_validator_1.IsString)(),
-    __metadata("design:type", String)
-], ContractDto.prototype, "matricula", void 0);
-__decorate([
-    (0, class_validator_1.IsDate)(),
-    __metadata("design:type", Date)
-], ContractDto.prototype, "beginDate", void 0);
-__decorate([
-    (0, class_validator_1.IsDate)(),
-    __metadata("design:type", Date)
-], ContractDto.prototype, "endDate", void 0);
-__decorate([
-    (0, class_validator_1.IsDate)(),
-    __metadata("design:type", Date)
-], ContractDto.prototype, "firmaDate", void 0);
-__decorate([
-    (0, class_validator_1.IsString)(),
-    __metadata("design:type", String)
-], ContractDto.prototype, "formaPago", void 0);
-__decorate([
-    (0, class_validator_1.IsBoolean)(),
-    __metadata("design:type", Boolean)
-], ContractDto.prototype, "seguro", void 0);
-__decorate([
-    (0, class_validator_1.IsInt)(),
-    (0, class_validator_1.Min)(0),
-    __metadata("design:type", Number)
-], ContractDto.prototype, "diasProrroga", void 0);
 //# sourceMappingURL=contract.dto.js.map
--- apps/api/src/contract/dto/contract.dto.ts
+++ apps/api/src/contract/dto/contract.dto.ts
@@ -1,28 +1,26 @@
 import { IsBoolean, IsDate, IsInt, IsString, Min, min } from "class-validator";
 
 export class ContractDto {
-    @IsString()
+  
     public idCliente : string;
 
-    @IsString()
+  
     public matricula : string;
 
-    @IsDate()
+  
     public beginDate : Date;
 
-    @IsDate()
+   
     public endDate : Date;
 
-    @IsDate()
+  
     public firmaDate : Date;
 
-    @IsString()
+   
     public formaPago : string;
 
-    @IsBoolean()
     public seguro : boolean;
 
-    @IsInt()
-    @Min(0)
+   
     public diasProrroga : number;
 }
