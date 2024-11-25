4442ae9fe3b477529248e4703e965f068b3d9557
JohnyYen <126342433+JohnyYen@users.noreply.github.com>
2024-11-25 18:03:15 -0500

    feature Proteced Routes

---
+++ apps/client/src/utils/ProtectedRoutes.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { Navigate } from 'react-router-dom';
+import { GlobalContext } from '../context/GlobalContext';
+
+function ProtectedRoutes({children}) {
+
+    const {isAuthenticate} = useContext(GlobalContext);
+
+    if (!isAuthenticate()) {
+        return <Navigate to="/home" replace />;
+      }
+    
+      return children;
+}
+
+export default ProtectedRoutes
